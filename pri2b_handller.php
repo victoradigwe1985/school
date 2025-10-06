9b01a1b93cd76feafaa61863679a14f1e996b102
victoradigwe1985 <victoradigwe1985@gmail.com>
2025-10-06 12:07:48 +0100

    Add files via upload

---
+++ pri2b_handller.php
@@ -0,0 +1,50 @@
+<?php
+
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+ if(isset($_POST["submit"])){
+            require("connection.php");
+            $file=addslashes(file_get_contents($_FILES["image"]["tmp_name"]));  
+            $registered_on=DATE("y-m-d h:i:s");
+            $firstname=sanitize($connect,$_POST["firstname"]);
+            $middlename=sanitize($connect,$_POST["middlename"]);
+            $surname=sanitize($connect,$_POST["surname"]);
+            $username=sanitize($connect,$_POST["username"]);
+            $email=sanitize($connect,$_POST["email"]);
+            $password=sanitize($connect,$_POST["password"]);
+            $gender=sanitize($connect,$_POST["gender"]);
+            $age=sanitize($connect,$_POST["age"]);
+            $class=sanitize($connect,$_POST["class"]);
+            $address=sanitize($connect,$_POST["address"]);
+            
+            $select="SELECT email FROM pri2b_register WHERE email='$email'";
+            if($query=mysqli_query($connect,$select)){
+                $num=mysqli_num_rows($query);
+                if($num > 0){
+                    $msg="Email <b>$email</b> already exists!";
+                    header("location:pri2b_form.php?msg=$msg");
+                }else{
+                        $insert="INSERT INTO pri2b_register(first_name,middle_name,surname,username,email,password,confirm_password,gender,age,class,address,registered_on,passport)VALUES('$firstname','$middlename','$surname','$username','$email','$password','$confirm_password','$gender','$age','$class','$address','$registered_on','$file')";
+                        if(mysqli_query($connect,$insert)){
+                            $msg="Data submitted successfully. Please Print Out this form because it will be require from you during clearance";
+                             header("location:pri2b_fetch_form.php?msg=$msg");
+                        }else{
+                            $msg="failed to exercute query on the database!";
+                             header("location:pri2b_form.php?msg=$msg");
+                        }
+                            }
+            }else{
+                $msg="failed to query database";
+                 header("location:pri2b_form.php?msg=$msg");
+            }
+  }else{
+      $msg="parameter missing! try again later";
+       header("location:pri2b_form.php?msg=$msg");
+  }
+                           
+            
+            ?>
---
+++ pri3a_assessment_third_update_1.php
@@ -0,0 +1,29 @@
+<?php
+if(isset($_POST['submit'])){
+    include_once("connection.php");
+    
+    $id=$_REQUEST["id"];
+   
+    $admission_no= $_POST["admission_no"];
+    $first_name= $_POST["first_name"];
+    $middle_name= $_POST["middle_name"];
+    $surname= $_POST["surname"];
+    $neatness=$_POST["neatness"];
+    $puntuality=$_POST["puntuality"];
+    $self_control=$_POST["self_control"];
+    $relationship=$_POST["relationship"];
+    $politeness=$_POST["politeness"];
+    $class_teacher_name=$_POST["class_teacher_name"];
+    $head_teacher_name=$_POST["head_teacher_name"];
+    $comment=$_POST["comment"];
+    
+    $update="UPDATE pri3a_assessment_third_register SET admission_no='".$admission_no."',first_name='".$first_name."',middle_name='".$middle_name."',neatness='".$neatness."',puntuality='".$puntuality."',self_control='".$self_control."',relationship='".$relationship."',politeness='".$politeness."',class_teacher_name='".$class_teacher_name."',head_teacher_name='".$head_teacher_name."',comment='".$comment."' WHERE register_id='".$id."'";
+    $query=mysqli_query($connect,$update);
+    if($query){
+        echo"<script type='text/javascript'>alert('Record Added Successfully');window.location='pri3a_assessment_third.php';</script>";
+    }else{
+       echo"<script type='text/javascript'>alert('Record Added Unsuccessful');window.location='pri3a_assessment_third.php';</script>";  
+    }
+    
+}
+?>
